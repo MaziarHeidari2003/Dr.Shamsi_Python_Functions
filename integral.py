5c27f0a45dfcefbc55ba4a70e6ca00bd8da858f6
MaziarHeidari2003 <maziarheidari1124@gmail.com>
2023-12-25 22:53:06 -0800

    end of the homework

---
+++ integral.py
@@ -0,0 +1,45 @@
+#PROBLEM 9
+
+
+def func(x):
+  """
+  its just a random mathematical func ,
+  we are going to use it to calcualate the integral
+  """
+  return 2*x+1
+
+
+def integral(a,b,n,func):
+
+  """
+  calaulates the integral. we should calcaulate the steps , we should use trapezoid method to calculate the integral
+  """
+  sum = 0
+  step = my_abs((a-b)) / n
+  x = a
+  while x < b:
+    if x == 0 or x == n:
+      sum += func(x)/2
+    else:  
+      sum += func(x) 
+    x += step
+  result = sum * step 
+  return result
+
+
+  
+def my_abs(x):
+  """
+    these function will return the absolute 
+    value of x  so that we 
+    dont have to deal with nagetive values
+  """
+  if x >= 0 :
+    return x
+  else:
+    return -x
+    
+
+
+result = integral(0,7,1000000,func)
+print(result)
